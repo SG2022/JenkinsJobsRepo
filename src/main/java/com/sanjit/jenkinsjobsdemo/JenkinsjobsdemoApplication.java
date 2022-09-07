5d0781cc46a5cca94cf654003485fd9a73a4ad2a
Sanjela <sanjela19@gmail.com>
2022-09-07 17:53:00 +0800

    changed java version

---
+++ src/main/java/com/sanjit/jenkinsjobsdemo/JenkinsjobsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sanjit.jenkinsjobsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsjobsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsjobsdemoApplication.class, args);
+	}
+
+}
